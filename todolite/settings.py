994e7bbf8eb877a094c8735faebe939cffd78803
Pavel Filippi <pavel.filippi@gmail.com>
2022-12-19 22:18:02 +0100

    Fix mypy errors
    * add type annotation
    * install type-stubs
      (https://github.com/typeddjango/django-stubs)

--- todolite/settings.py
+++ todolite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from typing import List
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-qr_yz@#3sk1#bndgf$n2!+rz0*0h%*z_k9faum=$k_myj$#(ha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
