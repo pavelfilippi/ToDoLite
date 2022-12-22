e454f8e42274e554c84c9afa5e66c9eb8d5f45ab
Pavel Filippi <pavel.filippi@gmail.com>
2022-12-22 22:41:57 +0100

    Add registration view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Profile
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data["password2"])
+            # Save the user object
+            new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
