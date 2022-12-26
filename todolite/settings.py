3264ab44c7ef4677fbba387fb2e4dbffbdded06a
Pavel Filippi <pavel.filippi@gmail.com>
2022-12-26 21:04:20 +0100

    Define custom auth backend - email auth

--- todolite/settings.py
+++ todolite/settings.py
@@ -133,3 +133,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = "tasks:home"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authentication.EmailAuthBackend",
+]
