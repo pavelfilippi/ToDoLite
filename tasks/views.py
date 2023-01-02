3df0c5777a6e9c994438782937fe08e02c174918
Pavel Filippi <pavel.filippi@gmail.com>
2023-01-02 22:39:37 +0100

    Implement a view for creating tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, HttpResponseRedirect
+
+from .forms import TaskForm
 
 
 def home(request):
@@ -6,3 +9,18 @@ def home(request):
         request,
         "tasks/home.html",
     )
+
+
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = request.user.profile
+            task.save()
+            messages.success(request, "New task created.")
+            # TODO: Don't redirect to home, but to list when ready
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = TaskForm()
+    return render(request, "tasks/create.html", {"form": form})
