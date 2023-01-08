6b5c4cdb10eb203c4205299c6653ce200ba62e2f
Pavel Filippi <pavel.filippi@gmail.com>
2023-01-08 20:33:33 +0100

    Implement editing form

--- tasks/forms.py
+++ tasks/forms.py
@@ -17,3 +17,24 @@ class TaskForm(forms.ModelForm):
         if due_date and due_date < timezone.now().date():
             raise forms.ValidationError("The due date cannot be in the past.")
         return due_date
+
+
+class TaskEditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].required = False
+        self.fields["due_date"].required = False
+        self.fields["completed"].required = False
+
+    class Meta:
+        model = Task
+        fields = ["title", "due_date", "completed"]
+        widgets = {
+            "due_date": forms.DateInput(attrs={"class": "datepicker", "type": "date"}),
+        }
+
+    def clean_due_date(self):
+        due_date = self.cleaned_data["due_date"]
+        if due_date and due_date < timezone.now().date():
+            raise forms.ValidationError("The due date cannot be in the past.")
+        return due_date
