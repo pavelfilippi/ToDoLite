c1157c7b6297c0dfa5d028abdba5fac187c72cba
Pavel Filippi <pavel.filippi@gmail.com>
2023-01-19 21:32:31 +0100

    Modify admin site for managing users
    - make last login & date joined read only
    - add Profile as inline to user admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from account.models import Profile
+
+
+admin.site.unregister(User)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ["date_joined", "last_login"]
+
+    inlines = [ProfileInline]
